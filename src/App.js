e8ad3c0a0324602cdcb4c2f445de5540ac79103a
Phantather <akdil.hstech@gmail.com>
2023-02-08 19:53:06 +0600

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from "react-router-dom"
+import Layout from "./Layout/Layout";
+import Home from "./pages/Home/Home";
+import "./styles/style.scss"
+import Register from "./pages/Register/Register";
+import Favorites from "./pages/Favorites/Favorites";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path={''} element={<Layout/>}>
+            <Route path={'/'} element={<Home/>}/>
+            <Route path={'/register'} element={<Register/>}/>
+            <Route path={'/favorites'} element={<Favorites/>}/>
+            <Route path={'/cart'} element={<Cart/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
